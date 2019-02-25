b2a7930f9cce7da7531dc9877d772059a5f89f44
people30 <people@live.jp>
2019-02-26 08:35:46 +0900

    初期化

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        // MySql 5.7.7 以前のアレ
+        \Schema::defaultStringLength(191);
+    }
+}
