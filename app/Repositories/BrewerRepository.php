f04d007feec9edf73bdd19c4222e3f089667fbac
people30 <people@live.jp>
2019-03-01 20:03:43 +0900

    BrewerRepository の実装を追加

--- app/Repositories/BrewerRepository.php
+++ app/Repositories/BrewerRepository.php
@@ -8,42 +8,94 @@ namespace App\Repositories
 
     class BrewerRepository implements IBrewerRepository
     {
-        public function __construction()
-        {
+        protected $areaRepository;
 
+        public function __construct(IAreaRepository $ara)
+        {
+            $this->areaRepository = $ara;
         }
 
         public function find(array $where) : Models\Brewer
         {
-
+            return $this->findAll($where)->first();
         }
 
         public function findAll(array $where) : Collection
         {
+            $query = $this->select();
 
+            if(array_key_exists('id') && is_int($where['id']))
+            {
+                $this->where('id', $where['id']);
+            }
+
+            $items = $query->get();
+            $items = $this->factory($items);
+
+            return $items;
         }
         
-        public function getAreal(Models\Area $area): Models\Brewer
+        public function getAreal(Models\Area $area) : Collection
         {
+            $items =
+                $this->select()
+                ->where('area_id', $area->id)
+                ->get();
 
+            $items = $this->factory($items);
+
+            return $items;
         }
         
         public function getRange(string $key, array $values) : Collection
         {
+            if(!($key == 'id' || $key == 'slug')) throw new \InvalidArgumentException('key');
+            
+            $items =
+                $this->select()
+                ->whereIn($key, $values)
+                ->get();
 
+            $items = $this->factory($items);
+
+            return $items;
         }
         
         public function getRandom() : Models\Brewer
         {
+            $items =
+                $this->select()
+                ->orderByRaw('rand()')
+                ->limit(1)
+                ->get();
 
+            $item = $this->factory($items)->first();
+
+            return $item;
         }
         
         public function getAll() : Collection
         {
             $items =
                 $this->select()
-                ->get()
-                ->map(function($i) { return Factory::factory(Models\Brewer::class, $i); });
+                ->get();
+
+            $items = $this->factory($items);
+
+            return $items;
+        }
+
+        protected function factory(Collection $items) : Collection
+        {
+            $areaIds = $items->map(function($i) { return $i->areaId; })->unique()->toArray();
+            $areas = $this->areaRepository->getRange('id', $areaIds);
+            $items = $items->map(function($i) use ($areas)
+            {
+                $i->area = $areas->first(function($a) use ($i) { return $a->id == $i->areaId; });
+                $i->address = $i->prefecture . $i->city . $i->town;
+
+                return Factory::factory(Models\Brewer::class, $i);
+            });
 
             return $items;
         }
@@ -62,13 +114,14 @@ namespace App\Repositories
                     'brewers.fax_number as faxNumber',
                     'brewers.email',
                     'prefectures.name as prefecture',
+                    'cities.area_id as areaId',
                     'cities.name as city',
                     'brewers.town',
                     'brewers.lat',
                     'brewers.lon',
                     'brewers.owner',
                     'brewers.toji',
-                    'brewers.buisiness_day as buisinessDays',
+                    'brewers.buisiness_day as buisinessDay',
                     'brewers.opening_time as openingTime',
                     'brewers.closing_time as closingTime',
                     'brewers.is_backstage_seeable as isBackstageSeeable',
--- app/Repositories/IBrewerRepository.php
+++ app/Repositories/IBrewerRepository.php
@@ -9,7 +9,7 @@ namespace App\Repositories
     {
         public function find(array $where) : Models\Brewer;
         public function findAll(array $where) : Collection;
-        public function getAreal(Models\Area $area): Models\Brewer;
+        public function getAreal(Models\Area $area) : Collection;
         public function getRange(string $key, array $values) : Collection;
         public function getRandom() : Models\Brewer;
         public function getAll() : Collection;
