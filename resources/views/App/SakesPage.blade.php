5134946204cff5ca3fb89035b6598da48b488913
people30 <people@live.jp>
2019-03-11 16:40:03 +0900

    SakesPage で brewers_page_script.js を読み込んでいたのを削除

--- resources/views/App/SakesPage.blade.php
+++ resources/views/App/SakesPage.blade.php
@@ -23,11 +23,10 @@
         <!-- jQuery -->
         <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
         <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAMUsphC2nSkQJ6Gq240PD0MyAt0EXSbJ4&callback=initMap" type="text/javascript"></script>
-        <script type="text/javascript" src="{{ asset('/js/brewers_page_script.js') }}"></script>
         <!-- ベーススクリプト -->
-        <script src="{{ asset('js/base.js') }}"></script>
+        <script src="{{ asset('/js/base.js') }}"></script>
         <!-- 地酒ページスクリプト -->
-        <script src="{{ asset('js/sakes_page.js') }}"></script>
+        <script src="{{ asset('/js/sakes_page.js') }}"></script>
         <title>ぐびっと:徳島の地酒</title>
 
     </head>
