8c953f27cbd81c178709e01dd971c84a78f35d72
people30 <people@live.jp>
2019-03-12 10:50:40 +0900

    RegionalityPage と BrewerDetailsPage を復旧

---
+++ resources/views/App/RegionalityPage.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>RegionalityPage</title>
+    </head>
+    <body>
+        <h1>RegionalityPage</h1>
+        <h2>$allBrewers</h2>
+        <p><pre><?php print_r($allBrewers) ?></pre></p>
+    </body>
+</html>
